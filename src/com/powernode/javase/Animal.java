1285b884b63b6529b7f620d1f91ba1f53c3c617f
Scarlett293 <2936900025@qq.com>
2024-04-30 10:40:22 +0800

    one

---
+++ src/com/powernode/javase/Animal.java
@@ -0,0 +1,12 @@
+package com.powernode.javase;
+
+/**
+ * ClassName: Animal
+ * Decription:
+ * Package: com.powernode.javase
+ * Date: 2024/4/30 8:07
+ * Author: Scalett
+ * Version: 1.0
+ */
+public class Animal {
+}
